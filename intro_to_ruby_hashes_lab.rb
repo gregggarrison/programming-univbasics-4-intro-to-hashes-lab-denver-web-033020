3c0a168e600c01c3eeebbcc0db8c5c2a7491f7dd
Gregg Garrison <gregg.garrison@rocketmail.com>
2020-03-14 21:36:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,9 +29,9 @@ end
 
 def update_counting_hash(hash, key)
  if hash[key]
-   hash[key] =+ 1 
+  update_counting_hash =  hash[key] =+ 1 
  else
-   hash[key] = 1 
+  update_hash = hash[key] = 1 
  end 
   
   
