65d7f12986343339852d1cd5945b3877755db3aa
Gregg Garrison <gregg.garrison@rocketmail.com>
2020-03-14 21:46:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,18 +28,13 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
- if hash[key] == {}
-   return hash[key] = 1 
+ if hash[key]
+  update_counting_hash =hash[key] =+ 1 
  else
-  hash[key] += 1 
+  hash[key] = 1 
  end 
   
-#   * if shipping_manifest["top hat"]
-# *   shipping_manifest["top hat"] += 1
-# * else
-# *   shipping_manifest["top hat"] = 1
-# * end
-
+  
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
