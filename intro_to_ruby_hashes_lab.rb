42306f26a415ab01c4493e0e7f91cb167ae25852
Gregg Garrison <gregg.garrison@rocketmail.com>
2020-03-14 21:55:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,8 +28,8 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
- if hash[key]
-  update_counting_hash =hash[key] += 1 
+ if hash[key] == []
+  update_counting_hash = hash[key] += 1 
  else
   update_hash = hash[key] = 1 
  end 
